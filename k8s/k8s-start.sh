139c327d54e997e4d3f4fcddbc1d6cd4f4188fcd
xhe <xw897002528@gmail.com>
2022-10-22 11:10:24 +0800

    k8s: update scripts
    Signed-off-by: xhe <xw897002528@gmail.com>

--- k8s/k8s-start.sh
+++ k8s/k8s-start.sh
@@ -12,13 +12,12 @@ export HELM=helm
 if [ -n "$1" ]; then
 	$HELM repo add pingcap https://charts.pingcap.org/ || true
 	$HELM repo add chaos-mesh https://charts.chaos-mesh.org/ || true
-	$KUBECTL create -f https://raw.githubusercontent.com/pingcap/tidb-operator/$TIDBOP_VERSION/manifests/crd.yaml || true
 	$HELM repo update
+	$KUBECTL apply -f https://raw.githubusercontent.com/pingcap/tidb-operator/$TIDBOP_VERSION/manifests/crd.yaml || true
 fi
 
 $KUBECTL delete namespace $NAMESPACE
 $KUBECTL create namespace $NAMESPACE || true
-$HELM install operator pingcap/tidb-operator --namespace $NAMESPACE --version $TIDBOP_VERSION
-$HELM install cluster pingcap/tidb-cluster --namespace $NAMESPACE --version $TIDBOP_VERSION --set \
-  "schedulerName=default-scheduler,pd.storageClassName=standard,tikv.storageClassName=standard,pd.replicas=1,tikv.replicas=1,tidb.replicas=1"
+$HELM install operator pingcap/tidb-operator --namespace $NAMESPACE --version $TIDBOP_VERSION --set "operatorImage=xhebox/operator:latest"
 $HELM install chaos chaos-mesh/chaos-mesh --namespace $NAMESPACE --version $CHAOS_VERSION
+$KUBECTL apply -f ./cluster.yaml
