837a1a3e87d874fbbaf62db3bec7972d4dbf0cdb
xhe <xw897002528@gmail.com>
2023-02-09 13:50:51 +0800

    k8s: add dashboard and monitor
    Signed-off-by: xhe <xw897002528@gmail.com>

--- k8s/cluster-start.sh
+++ k8s/cluster-start.sh
@@ -3,8 +3,8 @@
 set -ex
 
 export KUBECTL="kubectl"
-export TIDBOP_VERSION="v1.3.7"
-export TIDBOP_CRD_VERSION="ae22ce0dbd6b99fae993646ec30a198e0a63bc41"
+export TIDBOP_VERSION="v1.4.2"
+export TIDBOP_CRD_VERSION="e9876f702a8cabd20e49027e26df1daefebf1054"
 export CHAOS_VERSION="2.3.0"
 export NAMESPACE="testing"
 export HELM=helm
@@ -20,7 +20,9 @@ fi
 $KUBECTL delete --force namespace $NAMESPACE || true
 $KUBECTL create namespace $NAMESPACE || true
 $KUBECTL create secret generic basic-sess --namespace=$NAMESPACE --from-file=crt=cert.pem --from-file=key=key.pem
-$HELM install operator pingcap/tidb-operator --namespace $NAMESPACE --version $TIDBOP_VERSION --set "operatorImage=xhebox/tidb-operator:latest"
+$HELM install operator pingcap/tidb-operator --namespace $NAMESPACE --version $TIDBOP_VERSION --set "operatorImage=xhebox/tidb-operator:latest,imagePullPolicy=Always"
 #$HELM install chaos chaos-mesh/chaos-mesh --namespace $NAMESPACE --version $CHAOS_VERSION
 $KUBECTL apply -n $NAMESPACE -f ./cluster.yaml
+$KUBECTL apply -n $NAMESPACE -f ./monitor.yaml
+$KUBECTL apply -n $NAMESPACE -f ./dashboard.yaml
 $KUBECTL apply -n $NAMESPACE -f ./debug-proxy.yaml
